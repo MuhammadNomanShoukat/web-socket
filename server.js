d112d73b266a7ffed86e757ac9a63bb764d4d232
MuhammadNomanShoukat <nomanshoukat53@gmail.com>
2024-08-16 21:09:52 +0500

    route added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const ws = require("ws");
+const path = require("path")
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,10 @@ app.use(cors({ extended: true }));
 // HTTP routes
 app.use("/video/generateLink", require("./routes/video/generateLink.js"));
 app.use("/user/jwtAuth", require("./routes/user/userAuth.js"));
+
+app.get('/connect', (req, res)=>{
+  res.sendFile(path.join(__dirname, "index.html"))
+})
 
 // Create an HTTP server from the express app
 const server = app.listen(PORT, () => {
